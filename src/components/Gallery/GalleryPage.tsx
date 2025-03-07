1a5d5752bea4eacf7fb575a8f7fd40ee6d31d5cc
Yeabsira Tarekegn <yabahane@gmail.com>
2025-03-07 06:03:08 +0300

    added gallery page

--- src/components/Gallery/GalleryPage.tsx
+++ src/components/Gallery/GalleryPage.tsx
@@ -1,11 +1,380 @@
 import DescCard from "../DescCard";
 import img0 from "@/assets/Mount.jpeg";
+import img1 from "@/assets/old/thumb_1456765353NATIONAL_PRESIDENT.jpg.jpg";
+import img2 from "@/assets/old/thumb_1332792758SAM_1292.jpg.jpg";
+import img3 from "@/assets/old/thumb_1332792665SAM_1267.jpg.jpg";
+import img4 from "@/assets/old/thumb_1274624035PIC_1585.jpg.jpg";
+import img5 from "@/assets/old/thumb_1274623807PIC_1592.jpg.jpg";
+import img7 from "@/assets/old/thumb_1274623422PIC_1586.jpg.jpg";
+import img8 from "@/assets/old/thumb_1274623322PIC_1585.jpg.jpg";
+import img9 from "@/assets/old/thumb_1274623222PIC_1582.jpg.jpg";
+import img10 from "@/assets/old/thumb_1273521386050710163535.jpg.jpg";
+import img11 from "@/assets/old/thumb_1269461690PIC_1120.jpg.jpg";
+import img12 from "@/assets/old/thumb_1266837371PIC_0723.jpg.jpg";
+import img13 from "@/assets/old/thumb_1266836964PIC_0667.jpg.jpg";
+import img14 from "@/assets/old/thumb_1266836721PIC_0660.jpg.jpg";
+import img15 from "@/assets/old/thumb_1266836002PIC_0792.jpg.jpg";
+import img16 from "@/assets/old/thumb_1266835452PIC_0644.jpg.jpg";
+import img17 from "@/assets/old/thumb_1265162912PIC_0468.jpg.jpg";
+import img18 from "@/assets/old/thumb_1265127789PIC_0463.jpg.jpg";
+import img19 from "@/assets/old/thumb_1265127282PIC_0462.jpg.jpg";
+import img20 from "@/assets/old/thumb_1265127054PIC_0460.jpg.jpg";
+import img21 from "@/assets/old/thumb_1265125415PIC_0455.jpg.jpg";
+import img22 from "@/assets/old/thumb_1265124481PIC_0645.JPG.jpg";
+import img23 from "@/assets/old/thumb_1265123640PIC_0452.jpg.jpg";
+import img24 from "@/assets/old/thumb_1264629563PIC_0485.jpg.jpg";
+import img25 from "@/assets/old/thumb_1264628830PIC_0486.jpg.jpg";
+import img26 from "@/assets/old/thumb_1264628645PIC_0489.jpg.jpg";
+import img27 from "@/assets/old/thumb_1264626109PIC_0490.jpg.jpg";
+import img28 from "@/assets/old/thumb_1264626004PIC_0491.jpg.jpg";
+import img29 from "@/assets/old/thumb_1264625873PIC_0546.jpg.jpg";
+import img30 from "@/assets/old/thumb_1264625785PIC_0547.jpg.jpg";
+import img31 from "@/assets/old/thumb_1263322707PIC_0272.jpg.jpg";
+import img32 from "@/assets/old/thumb_1263322404PIC_0270.jpg.jpg";
+import img33 from "@/assets/old/thumb_1263322164PIC_0265.jpg.jpg";
+import img34 from "@/assets/old/thumb_1263322017PIC_0264.jpg.jpg";
+import img35 from "@/assets/old/thumb_1263321747PIC_0263.jpg.jpg";
+import img36 from "@/assets/old/thumb_1263320122PIC_0262.jpg.jpg";
+import img37 from "@/assets/old/thumb_1263319370PIC_0267.jpg.jpg";
+import img38 from "@/assets/old/thumb_1263317998PIC_0260.jpg.jpg";
+import img39 from "@/assets/old/thumb_1263316151PIC_0146.jpg.jpg";
+import img40 from "@/assets/old/thumb_1263290386PIC_0137.jpg.jpg";
+import img42 from "@/assets/old/thumb_1262884631PIC_0133.jpg.jpg";
+import img43 from "@/assets/old/thumb_1262884492PIC_0132.jpg.jpg";
+import img44 from "@/assets/old/thumb_1262883873PIC_0131.jpg.jpg";
+import img45 from "@/assets/old/thumb_1262883603PIC_0130.jpg.jpg";
+import img46 from "@/assets/old/thumb_1262883425PIC_0124.jpg.jpg";
+import img47 from "@/assets/old/thumb_1262798334PIC_0314.jpg.jpg";
+import img48 from "@/assets/old/thumb_1262798014PIC_0307.jpg.jpg";
+import img49 from "@/assets/old/thumb_1262797847PIC_0250.jpg.jpg";
+import img50 from "@/assets/old/thumb_1262797712PIC_0248.jpg.jpg";
+import img51 from "@/assets/old/thumb_1262797532PIC_0224.jpg.jpg";
+import img52 from "@/assets/old/thumb_1262796901PIC_0251.jpg.jpg";
+import img53 from "@/assets/old/thumb_1262796818PIC_0203.jpg.jpg";
+import img54 from "@/assets/old/thumb_1262796585PIC_0249.jpg.jpg";
+import img55 from "@/assets/old/thumb_1262795852PIC_0243.jpg.jpg";
+import img56 from "@/assets/old/thumb_1262795654PIC_0199.jpg.jpg";
+import img57 from "@/assets/old/thumb_1262795380PIC_0196.jpg.jpg";
+import img58 from "@/assets/old/thumb_1262795164PIC_0188.jpg.jpg";
+import img59 from "@/assets/old/thumb_1262794785PIC_0177.jpg.jpg";
+import img60 from "@/assets/old/thumb_1262794053PIC_0182.jpg.jpg";
+import img61 from "@/assets/old/thumb_1262777300PIC_0171.jpg.jpg";
+import img62 from "@/assets/old/thumb_1262776433PIC_0169.jpg.jpg";
+import img63 from "@/assets/old/thumb_1262776285PIC_0168.jpg.jpg";
+import img64 from "@/assets/old/thumb_1262776047PIC_0155.jpg.jpg";
+import img65 from "@/assets/old/thumb_1262775906PIC_0154.jpg.jpg";
+import img66 from "@/assets/old/thumb_1262775575PIC_0120.jpg.jpg";
+import img67 from "@/assets/old/thumb_1262775453PIC_0119.jpg.jpg";
+import img68 from "@/assets/old/thumb_1262775206PIC_0118.jpg.jpg";
+import img69 from "@/assets/old/thumb_1262775050PIC_0117.jpg.jpg";
+import img70 from "@/assets/old/thumb_1262774889PIC_0115.jpg.jpg";
+import img71 from "@/assets/old/thumb_1262774704PIC_0108.jpg.jpg";
+import img72 from "@/assets/old/thumb_1262773110IMG0331A.jpg.jpg";
+import img73 from "@/assets/old/thumb_1262770131Image019.jpg.jpg";
+import CircularGallery from "../ui/CircularGallery";
 
 const GalleryPage = () => {
+
+const images = [
+  {
+    image: img1,
+    text: "Image 1",
+  },
+  {
+    image: img2,
+    text: "Image 2",
+  },
+  {
+    image: img3,
+    text: "Image 3",
+  },
+  {
+    image: img4,
+    text: "Image 4",
+  },
+  {
+    image: img5,
+    text: "Image 5",
+  },
+  {
+    image: img16,
+    text: "Image 6",
+  },
+  {
+    image: img7,
+    text: "Image 7",
+  },
+  {
+    image: img8,
+    text: "Image 8",
+  },
+  {
+    image: img9,
+    text: "Image 9",
+  },
+  {
+    image: img10,
+    text: "Image 10",
+  },
+  {
+    image: img11,
+    text: "Image 11",
+  },
+  {
+    image: img12,
+    text: "Image 12",
+  },
+  {
+    image: img13,
+    text: "Image 13",
+  },
+  {
+    image: img14,
+    text: "Image 14",
+  },
+  {
+    image: img15,
+    text: "Image 15",
+  },
+  {
+    image: img16,
+    text: "Image 16",
+  },
+  {
+    image: img17,
+    text: "Image 17",
+  },
+  {
+    image: img18,
+    text: "Image 18",
+  },
+  {
+    image: img19,
+    text: "Image 19",
+  },
+  {
+    image: img20,
+    text: "Image 20",
+  },
+  {
+    image: img21,
+    text: "Image 21",
+  },
+  {
+    image: img22,
+    text: "Image 22",
+  },
+  {
+    image: img23,
+    text: "Image 23",
+  },
+  {
+    image: img24,
+    text: "Image 24",
+  },
+  {
+    image: img25,
+    text: "Image 25",
+  },
+  {
+    image: img26,
+    text: "Image 26",
+  },
+  {
+    image: img27,
+    text: "Image 27",
+  },
+  {
+    image: img28,
+    text: "Image 28",
+  },
+  {
+    image: img29,
+    text: "Image 29",
+  },
+  {
+    image: img30,
+    text: "Image 30",
+  },
+  {
+    image: img31,
+    text: "Image 31",
+  },
+  {
+    image: img32,
+    text: "Image 32",
+  },
+  {
+    image: img33,
+    text: "Image 33",
+  },
+  {
+    image: img34,
+    text: "Image 34",
+  },
+  {
+    image: img35,
+    text: "Image 35",
+  },
+  {
+    image: img36,
+    text: "Image 36",
+  },
+  {
+    image: img37,
+    text: "Image 37",
+  },
+  {
+    image: img38,
+    text: "Image 38",
+  },
+  {
+    image: img39,
+    text: "Image 39",
+  },
+  {
+    image: img40,
+    text: "Image 40",
+  },
+
+  {
+    image: img42,
+    text: "Image 42",
+  },
+  {
+    image: img43,
+    text: "Image 43",
+  },
+  {
+    image: img44,
+    text: "Image 44",
+  },
+  {
+    image: img45,
+    text: "Image 45",
+  },
+  {
+    image: img46,
+    text: "Image 46",
+  },
+  {
+    image: img47,
+    text: "Image 47",
+  },
+  {
+    image: img48,
+    text: "Image 48",
+  },
+  {
+    image: img49,
+    text: "Image 49",
+  },
+  {
+    image: img50,
+    text: "Image 50",
+  },
+  {
+    image: img51,
+    text: "Image 51",
+  },
+  {
+    image: img52,
+    text: "Image 52",
+  },
+  {
+    image: img53,
+    text: "Image 53",
+  },
+  {
+    image: img54,
+    text: "Image 54",
+  },
+  {
+    image: img55,
+    text: "Image 55",
+  },
+  {
+    image: img56,
+    text: "Image 56",
+  },
+  {
+    image: img57,
+    text: "Image 57",
+  },
+  {
+    image: img58,
+    text: "Image 58",
+  },
+  {
+    image: img59,
+    text: "Image 59",
+  },
+  {
+    image: img60,
+    text: "Image 60",
+  },
+  {
+    image: img61,
+    text: "Image 61",
+  },
+  {
+    image: img62,
+    text: "Image 62",
+  },
+  {
+    image: img63,
+    text: "Image 63",
+  },
+  {
+    image: img64,
+    text: "Image 64",
+  },
+  {
+    image: img65,
+    text: "Image 65",
+  },
+  {
+    image: img66,
+    text: "Image 66",
+  },
+  {
+    image: img67,
+    text: "Image 67",
+  },
+  {
+    image: img68,
+    text: "Image 68",
+  },
+  {
+    image: img69,
+    text: "Image 69",
+  },
+  {
+    image: img70,
+    text: "Image 70",
+  },
+  {
+    image: img71,
+    text: "Image 71",
+  },
+  {
+    image: img72,
+    text: "Image 72",
+  },
+  {
+    image: img73,
+    text: "Image 73",
+  },
+];
+
   return (
     <div className="mt-40">
       <DescCard title="Gallery" breadcrumb="gallery" backgroundImage={img0} />
-      <div className="container mx-auto"></div>
+      <div className=" mx-auto bg-green-500 h-[50vh]">
+        <div style={{ height: '350px', position: 'relative' }}>
+        <CircularGallery items={images} bend={4} textColor="#ffffff" borderRadius={0.05} />
+      </div>
+      </div>
     </div>
   );
 };
